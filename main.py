74e17a685c7158b002e04512f90ceee5d7af2705
strongscot <mail@strongscot.com>
2020-03-03 19:40:04 +0000

    - Added a few comments

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from SiteUrlCrawler import SiteUrlCrawler
 
 
 def main():
+    # Create a site crawler with 5 threads and allowing logging
     crawler = SiteUrlCrawler("https://strongscot.com", 5, True)
 
+    # Get all the URLs and print them
     for url in crawler.crawl():
         print("Found: " + url)
 
